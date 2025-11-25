f445aef25d863e0c1d6c5b82f08e0a3454d8c2b7
Modernize Studio <studio@modernize.com>
2025-11-25 05:56:22 +0000

    Commit for planId: 358

--- src/main/java/crm/WebAppConfig.java
+++ src/main/java/crm/WebAppConfig.java
@@ -12,25 +12,25 @@ import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.dialect.springdata.SpringDataDialect;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Configuration
-public class WebAppConfig extends WebMvcConfigurerAdapter {
+public class WebAppConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -49,17 +49,16 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorPathExtension(true)
+        configurer.favorPathExtension(false)
                 .ignoreAcceptHeader(false)
-                .defaultContentType(MediaType.APPLICATION_JSON)
-                .useJaf(false);
+                .defaultContentType(MediaType.APPLICATION_JSON);
 
         final Map<String,MediaType> mediaTypes = new HashMap<>();
         mediaTypes.put("html", MediaType.TEXT_HTML);
         mediaTypes.put("json", MediaType.APPLICATION_JSON);
         mediaTypes.put("xls", MediaType.valueOf("application/vnd.ms-excel"));
         mediaTypes.put("pdf", MediaType.APPLICATION_PDF);
-        mediaTypes.put("csv", new MediaType("text","csv", Charset.forName("utf-8")));
+        mediaTypes.put("csv", new MediaType("text","csv", StandardCharsets.UTF_8));
         configurer.mediaTypes(mediaTypes);
     }
 
@@ -92,7 +91,7 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
         templateResolver.setPrefix("templates/");
         templateResolver.setCacheable(false);
         templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setTemplateMode("HTML");
         templateResolver.setCharacterEncoding("UTF-8");
 
         return templateResolver;
@@ -104,18 +103,9 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
-
-        // add dialect spring security
+        templateEngine.addDialect(new Java8TimeDialect());
         templateEngine.addDialect(new SpringSecurityDialect());
         return templateEngine;
-    }
-
-    @Bean
-    public TemplateEngine templateEngine(ITemplateResolver templateResolver) {
-        SpringTemplateEngine engine = new SpringTemplateEngine();
-        engine.addDialect(new Java8TimeDialect());
-        engine.setTemplateResolver(templateResolver);
-        return engine;
     }
 
     @Bean
@@ -155,11 +145,6 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
     @Bean
     public ViewResolver pdfViewResolver() {
         return new PdfViewResolver();
-    }
-
-    @Bean
-    public SpringDataDialect springDataDialect() {
-        return new SpringDataDialect();
     }
 
 }
