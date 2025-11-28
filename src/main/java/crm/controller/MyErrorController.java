193e6bf9220c20d4b587abd886c7edd6b11bbd4c
Modernize Studio <studio@modernize.com>
2025-11-28 17:07:30 +0000

    Commit for planId: 24

--- src/main/java/crm/controller/MyErrorController.java
+++ src/main/java/crm/controller/MyErrorController.java
@@ -1,8 +1,11 @@
 package crm.controller;
 
-import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class MyErrorController implements ErrorController {
@@ -10,13 +13,13 @@ public class MyErrorController implements ErrorController {
     private static final String PATH = "/error";
 
     @RequestMapping(value = PATH)
-    public String error() {
+    public String error(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            return "Error " + statusCode + ": Error handling";
+        }
         return "Error handling";
-    }
-
-    @Override
-    public String getErrorPath() {
-        return PATH;
     }
 
 }
